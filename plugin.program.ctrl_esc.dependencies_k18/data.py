087ad2f3c0ddcc00115c0ddee121e820899b8399
tox303 <36279834+tox303@users.noreply.github.com>
2022-11-21 01:49:15 +0100

    Elfriede

--- plugin.program.ctrl_esc.dependencies_k18/data.py
+++ plugin.program.ctrl_esc.dependencies_k18/data.py
@@ -87,7 +87,7 @@ script.module.thetvdb-1.0.27*https://bit.ly/3xfGKFp*module.png
 script.module.F4mProxy-2.8.7*https://bit.ly/3Rsu2dK*module.png
 script.module.tzlocal-2.0.0*https://bit.ly/3q5Lj0L*module.png
 
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 script.module.urlresolver-2021.12.23*https://bit.ly/3OyiZ1f*urlresolver.png
 script.module.resolveurl-5.1.75.nightly1*https://bit.ly/3wsEN7Z*resolve_url.png
 
@@ -117,14 +117,14 @@ script.module.resolveurl-5.1.80*https://bit.ly/3shZ1i4*resolve_url.png
 
 
 
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 
 
 plugin.video.live.streamspro-2.8.1*https://bit.ly/3gwEA9U*LSPro.png
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 EPG Repository DeBaschdi-1.0.1*https://bit.ly/3ydqBRF*takealug.png
 service.takealug.epg-grabber-0.3.8*https://bit.ly/3yaCeJ2*takealug.png
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 
 repository.yt.testing_official-1.0.1*https://bit.ly/3tPTwIT*youtube.png
 repository.yt.testing_unofficial-1.0.1*https://bit.ly/3n84QvZ*youtube.png
@@ -137,13 +137,13 @@ Youtube-6.8.3.alpha1*https://bit.ly/3N8gFwU*youtube.png
 
 Unofficial Youtube-6.8.19~alpha1*https://bit.ly/3KLBPS8*youtube.png
 
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 
 
 repository.infinity-1.0.0*https://bit.ly/3NdJ30G*Infinity.png
 script.infinity.artwork-1.0.1*https://bit.ly/2DpBBBN*Infinity.png
 plugin.video.infinity-1.1.2*https://bit.ly/3xLE1CL*Infinity.png
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 
 repository.lastship-1.0.2*https://bit.ly/3xOXq60*lastship.png
 repository.lastship-1.0.6*https://bit.ly/3ybG6Jz*lastship.png
@@ -152,50 +152,50 @@ plugin.video.lastship-4.0.6.4*https://bit.ly/3NmjbQs*lastship.png
 Unofficial plugin.video.lastship-4.0.6.5*https://bit.ly/3QFdeRc*lastship.png
 Unofficial plugin.video.lastship-4.0.6.5.1*https://bit.ly/3nbcqpS*lastship.png
 Unofficial plugin.video.lastship-4.0.7.1.1*https://bit.ly/3QDBhjv*lastship.png
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 
 
 repository.xstream-1.1.9*https://bit.ly/3bnb8VV*xStreamRepo.png
 plugin.video.xstream-3.5.35*https://bit.ly/3wM0XlL*x_addon.png
 xstream-script.module.resolveurl-5.1.74*https://bit.ly/3pWVR27*resolve_url.png
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 
 MyTV-STB-MAG-Client-4.8.1*https://bit.ly/3AJSOQ2*ctrl_esc_mytv.png
 MyTV-MAC-Client-4.8.1*https://bit.ly/3CPiTzP*ctrl_esc_mytv_mac.png
 
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 
 script.module.universalscrapers-1.2.3*https://bit.ly/3VMs2A0*universal.png
 repository.universalscrapers-1.0.0*https://bit.ly/2IVZ8bl*universal.png
 
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 
 Video-Jackson-API-Service_Module-4.8.1*https://bit.ly/3Q969Hb*service._v.png
 Video-Jackson-API-Interface-4.8.1*https://bit.ly/3ee9l7b*service._v_addon.png
 
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 CastagnaIT Repository for Kodi 18-1.0.0*https://bit.ly/3Vcz94I*CastagnaIT.jpg
 CastagnaIT Repository-2.0.0*https://bit.ly/3rF7FqO*CastagnaIT.jpg
 
 CastagnaIT Netfilx Addon-1.12.12*https://bit.ly/3rEqkD9*CastagnaIT.jpg
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 
 Sandmann79 Repository-1.0.4*https://bit.ly/3Cm0Zmx*module.png
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 service.thoradia Pi3-9.2.0.23*https://bit.ly/39H5Lkd*thoradia.png
 service.thoradia Pi4-9.2.0.23*https://bit.ly/3NfN7h1*thoradia.png
 service.thoradia x86_64-9.2.0.23*https://bit.ly/3bpt6HH*thoradia.png
 
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 
 repository.skinbase.18-3.1.36*https://bit.ly/3f6gPdb*module.png
 plugin.audio.radio_de-3.0.3*https://bit.ly/3Dc0FHa*module.png
 plugin.audio.ramfm-2.1.0*https://mirrors.kodi.tv/addons/leia/plugin.audio.ramfm/plugin.audio.ramfm-2.1.0.zip*module.png
 plugin.audio.soundcloud-3.2.0*https://mirrors.kodi.tv/addons/leia/plugin.audio.soundcloud/plugin.audio.soundcloud-3.2.0.zip*module.png
 
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
 
 plugin.audio.ravearchive-4.8.1*https://bit.ly/3AwVz7R*rave_archive.png
+script.module.TheYid.common-0.1.2*https://bit.ly/3EKwngP*module.png
 
-
-[COLOR lime]===================================[/COLOR]*ext*module.png
+[COLOR lime]===================================[/COLOR]*ext*äümodule.png
